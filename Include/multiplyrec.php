e21dbaec68a4078292ac3f847d22bce3955d2871
Sander Cokart <112039@student.drenthecollege.nl>
2018-12-13 12:01:06 +0100

    indentation fixes

--- Include/multiplyrec.php
+++ Include/multiplyrec.php
@@ -2,10 +2,10 @@
 
 function multiplyrec($a, $b) {
  if ($a < 1 || $b < 1) {
- return 0;
+   return 0;
  } else {
- return multiplyrec($a-1, $b) + $b;
-}
+   return multiplyrec($a-1, $b) + $b;
+ }
 }
 
  ?>
